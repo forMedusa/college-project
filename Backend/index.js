5613b46d920992f759af7e114fa58ba4ff014ef1
forMedusa <amaan5054@gmail.com>
2022-04-23 16:33:12 +0530

    Update

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,8 @@
 const express= require('express');
 const cors= require('cors');
 const bodyparser= require('body-parser');
-const mysql= require('mysql2');
+//const mysql= require('mysql2');
+const mongoose=require('mongoose');
 const e = require('express');
 var coroptions={
     origin: "http://localhost:4200"
@@ -11,53 +12,66 @@ const app=express();
 app.use(cors());
 app.use(bodyparser.json());
 
-// Database Connection
+const port = process.env.PORT || 8080;
 
-const db= mysql.createConnection({
-    host: 'localhost',
-    user:'root',
-    password:'',
-    database:'college-project',
-    port: 3306
+// Database Connection
+mongoose.connect(`mongodb://localhost:27017/college-project`)
+.then(() => {
+  console.log('Connected to database');
+  app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+  });
 });
 
-//Check Database Connection
+// app.listen(3000,()=>{
+//     console.log('server running');
+// })
 
-db.connect(err=>{
-    if(err){
-        console.log('Error!');
-    }
-    else{
-        console.log('Database Connected!');
-    }
-})
+
+// Database Connection
+
+// const db= mysql.createConnection({
+//     host: 'localhost',
+//     user:'root',
+//     password:'',
+//     database:'college-project',
+//     port: 3306
+// });
+//Check Database Connection
+ 
+
+// db.connect(err=>{
+//     if(err){
+//         console.log('Error!');
+//     }
+//     else{
+//         console.log('Database Connected!');
+//     }
+// })
 
 // POST API
-app.post('/user',(req,res)=>{
-    console.log(req.body,'create data');
-    let uname= req.body.username;
-    let name= req.body.name;
-    let email=req.body.email;
-    let password=req.body.pass;
-    let institute= req.body.institute;
-    // let qr= 'INSERT INTO user(name,email,password,Institute)\n' +
-    // "     VALUES(${name},${email},${password},${institute})"
-    let qr= `INSERT INTO user(username,
-        name,email,password,institute) VALUES('${uname}','${name}','${email}','${password}','${institute}')`;
+// app.post('/user',(req,res)=>{
+//     console.log(req.body,'create data');
+//     let uname= req.body.username;
+//     let name= req.body.name;
+//     let email=req.body.email;
+//     let password=req.body.pass;
+//     let institute= req.body.institute;
+//     // let qr= 'INSERT INTO user(name,email,password,Institute)\n' +
+//     // "     VALUES(${name},${email},${password},${institute})"
+//     let qr= `INSERT INTO user(username,
+//         name,email,password,institute) VALUES('${uname}','${name}','${email}','${password}','${institute}')`;
 
-    db.query(qr,(err,result)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.send({
-                message: "Data Inserted!"
-            });
-        }
+//     db.query(qr,(err,result)=>{
+//         if(err){
+//             console.log(err);
+//         }
+//         else{
+//             res.send({
+//                 message: "Data Inserted!"
+//             });
+//         }
        
-    });
-})
+//     });
+// })
 
-app.listen(3000,()=>{
-    console.log('server running');
-})
